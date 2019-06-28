3b6eb04f9a7371ed20145e3d19ef85c36f462289
davralin <davralin@davralin.host>
2019-06-28 15:59:51 +0200

    Remove comments

--- i3blocks/scripts/wot.sh
+++ i3blocks/scripts/wot.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 /usr/bin/curl -s https://wotquotes.com/ | /bin/grep quotescollection-2 | /usr/bin/cut -d'>' -f4 | /usr/bin/cut -d'<' -f1 | /bin/sed -r -e 's/&#8230;/.../g' -e "s/&#8217;/'/g" -e 's/,\.\.\./\.\.\./g' -e 's/\. \. \. \./\.\.\.\./g' -e 's:<br />: :g' -
-#/usr/bin/curl -s https://www.matcauthon.com/ | /bin/grep quotescollection-5 | /bin/sed -r -e 's/&#8230;/.../g' -e "s/&#8217;/'/g" -e 's/,\.\.\./\.\.\./g' -e 's/\. \. \. \./\.\.\.\./g' -e 's:<br />: :g' - | /usr/bin/cut -d'>' -f4 | /usr/bin/cut -d'<' -f1
