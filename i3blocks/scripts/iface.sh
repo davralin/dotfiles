e15943bf2afb07fec46463e8ffa8107dc28eb19f
davralin <davralin@davralin.host>
2019-06-29 13:20:44 +0200

    Fix interface-config

--- i3blocks/scripts/iface.sh
+++ i3blocks/scripts/iface.sh
@@ -28,35 +28,22 @@ fi
 
 # As per #36 -- It is transparent: e.g. if the machine has no battery or wireless
 # connection (think desktop), the corresponding block should not be displayed.
-[[ ! -d /sys/class/net/${IF} ]] && exit
+#[[ ! -d /sys/class/net/${IF} ]] && exit
 
 #------------------------------------------------------------------------
 
-if [[ "$(cat /sys/class/net/$IF/operstate)" = 'down' ]]; then
-  echo down # full text
-  echo down # short text
+if [[ "$(cat /sys/class/net/$IF/operstate)" = 'up' ]]; then
+  echo ""
+  echo ""
+  echo \#00FF00
+elif [[ "$(cat /sys/class/net/$IF/operstate)" = 'unknown' ]]; then
+  echo ""
+  echo ""
+  echo \#00FF00
+else
+  echo ""
+  echo ""
   echo \#FF0000 # color
-  exit
 fi
 
-case $1 in
-  -4)
-    AF=inet ;;
-  -6)
-    AF=inet6 ;;
-  *)
-    AF=inet6? ;;
-esac
-
-# if no interface is found, use the first device with a global scope
-IPADDR=$(ip addr show $IF | perl -n -e "/$AF ([^\/]+).* scope global/ && print \$1 and exit")
-
-case $BLOCK_BUTTON in
-  3) echo -n "$IPADDR"
-# | xclip -q -se c ;;
-esac
-
 #------------------------------------------------------------------------
-
-echo up # full text
-echo up # short text
