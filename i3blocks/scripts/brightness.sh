1dd7ed7c5b6a1bd06db959aa5b9d3b1196c47e21
davralin <davralin@davralin.host>
2019-07-01 16:13:30 +0200

    Revert brightness

--- i3blocks/scripts/brightness.sh
+++ i3blocks/scripts/brightness.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
   /usr/bin/sudo /usr/bin/brightnessctl set 1% > /dev/null
+elif [[ "$BLOCK_BUTTON" -eq 3 ]]; then
+  /usr/bin/sudo /usr/bin/brightnessctl set 100% > /dev/null
 fi
 /usr/bin/brightnessctl | /bin/grep Current | /usr/bin/cut -d'(' -f2 | /usr/bin/cut -d')' -f1
