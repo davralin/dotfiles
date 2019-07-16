6c8fdc8529fb4573cc04403438e586e6f2a6ac49
davralin <davralin@davralin.host>
2019-07-16 12:40:39 +0200

    Rolling my own mediaplayer-thingy

--- i3blocks/scripts/media.sh
+++ i3blocks/scripts/media.sh
@@ -1,11 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
-  playerctl next
+  playerctl previous
 elif [[ "$BLOCK_BUTTON" -eq 2 ]]; then
   playerctl play-pause
 elif [[ "$BLOCK_BUTTON" -eq 3 ]]; then
-  playerctl previous
+  playerctl next
 fi
-ARTIST=$(playerctl metadata artist)
-TITLE=$(playerctl metadata title)
-echo "$ARTIST - $TITLE"
+
+
+STATUS=$(playerctl status 2>&1)
+if [[ $STATUS == "No players found" ]]; then
+  echo ""
+elif [[ $STATUS == "Stopped" ]]; then
+  echo ""
+elif [[ $STATUS == "Paused" ]]; then
+  ARTIST=$(playerctl metadata artist)
+  TITLE=$(playerctl metadata title)
+  echo " $ARTIST - $TITLE"
+else
+  ARTIST=$(playerctl metadata artist)
+  TITLE=$(playerctl metadata title)
+  echo "$ARTIST - $TITLE"
+fi
