3eb9efd07037f0a17637588969f98c268a9ed7a6
davralin <gpg@davralin.work>
2024-02-20 22:23:55 +0100

    Typo

--- nixos/includes/impermanence-root.nix
+++ nixos/includes/impermanence-root.nix
@@ -14,7 +14,7 @@
     = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
   environment.etc."ssh/ssh_host_ed25519_key.pub".source
     = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
-  etc."NetworkManager/system-connections".source
+  environment.etc."NetworkManager/system-connections".source
     = "/nix/persist/etc/NetworkManager/system-connections/";
   systemd.tmpfiles.rules = [
     "L /var/lib/bluetooth - - - - /nix/persist/var/lib/bluetooth"
