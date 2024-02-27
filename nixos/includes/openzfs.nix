b3495c516f9f34f6c723ee78fd21104d828c066c
davralin <gpg@davralin.work>
2024-02-27 15:46:03 +0100

    Dont keep empty snapshots for zfs

--- nixos/includes/openzfs.nix
+++ nixos/includes/openzfs.nix
@@ -4,5 +4,6 @@
   # Add zfs-support
   boot.supportedFilesystems = [ "zfs" ];
   services.zfs.autoSnapshot.enable = true;
+  services.zfs.autoSnapshot.flags = "-p --utc";
   services.zfs.autoScrub.enable = true;
 }
