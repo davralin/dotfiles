fc74d1dc81fa169f69e2c5d47bafa4f8191eddef
davralin <gpg@davralin.work>
2024-04-18 12:17:11 +0200

    feat(nixos): automate pulling down dotfiles-repo

--- nixos/includes/auto-update.nix
+++ nixos/includes/auto-update.nix
@@ -5,4 +5,26 @@
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
   nix.settings.auto-optimise-store = true;
+
+  # Automate git pull of dotfiles
+  systemd.timers."git-pull-dotfiles" = {
+    wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+        Unit = "git-pull-dotfiles.service";
+      };
+  };
+
+  systemd.services."git-pull-dotfiles" = {
+    script = ''
+      set -eu
+      cd /nix/persist/dotfiles
+      ${pkgs.git}/bin/git pull --rebase
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
 }
