7ed2d365adcafada64204a2c535c986ab2ccb6ef
davralin <gpg@davralin.work>
2024-06-14 18:40:03 +0200

    Add ansible-support for nixos

---
+++ nixos/includes/ansible.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  # Define a user account for ansible
+  users.users.ansible = {
+    isSystemUser = true;
+    description = "ansible";
+    group = "wheel";
+    shell = "${pkgs.bash}/bin/bash";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJPAMGtseoJt0rtkc2U8PM09W5AlCivFo646ZiUDnd0M" # AWX
+    ];
+  };
+  environment.systemPackages = with pkgs; [
+    python3
+  ];
+}
