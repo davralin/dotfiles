3d2ee62274944120bdbe28d1408c396c942e9720
davralin <gpg@davralin.work>
2024-06-22 15:20:40 +0200

    Easier sunshine

--- nixos/includes/sunshine.nix
+++ nixos/includes/sunshine.nix
@@ -1,39 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Add sunshine-package
-  environment.systemPackages = with pkgs; [
-    sunshine
-  ];
-
-  networking.firewall = {
-    allowedTCPPorts = [ 47984 47989 47990 48010 ];
-    allowedUDPPortRanges = [
-      { from = 47998; to = 48000; }
-    ];
-  };
-
-  # Requires to simulate input
-  boot.kernelModules = [ "uinput" ];
-  services.udev.extraRules = ''
-    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
-  '';
-
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
-
-  systemd.user.services.sunshine = {
-    description = "Sunshine";
-    serviceConfig = {
-      ExecStart = "${config.security.wrapperDir}/sunshine";
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
-    wantedBy = ["graphical-session.target"];
-    after = ["network-oline.target"];
+  # Enable sunshine
+  services.sunshine = {
+    enable = true;
+    openFirewall = true;
   };
 }
