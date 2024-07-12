0b575349d61833066d36108ebac87414c7648425
davralin <gpg@davralin.work>
2024-07-12 22:51:06 +0200

    Add examples to contact-info and such for tor_bridge

--- nixos/includes/tor_bridge.nix
+++ nixos/includes/tor_bridge.nix
@@ -10,6 +10,9 @@
       role = "bridge";
     };
     settings = {
+      #Address = "127.0.0.1";
+      #ContactInfo= "remove@spam.com";
+      #Nickname = "TorBridge";
       ORPort = 443;
       MetricsPort = "0.0.0.0:9035 prometheus";
       MetricsPortPolicy = "accept 0.0.0.0/0";
