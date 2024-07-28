fdea4cba74bc6f62c1a5b443f889402b63605cd5
davralin <gpg@davralin.work>
2024-07-28 11:32:16 +0200

    Remove tubearchivist, add music-assistant to home-nas

---
+++ nixos/includes/docker-musicassistant.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  systemd.services.docker-musicassistant = {
+    description = "Docker MusicAssistant";
+    serviceConfig = {
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f /opt/musicassistant/docker-compose.yml up";
+      Restart = "on-failure";
+    };
+    wantedBy = ["multi-user.target"];
+    after = ["docker.service" "docker.socket"];
+  };
+}
