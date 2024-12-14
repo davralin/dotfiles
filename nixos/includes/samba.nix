ef062bfdbbac396d573af7de9540ebda492aced2
davralin <gpg@davralin.work>
2024-12-14 10:17:58 +0100

    Do thing with samba

--- nixos/includes/samba.nix
+++ nixos/includes/samba.nix
@@ -6,13 +6,7 @@
   services.samba = {
     enable = true;
     openFirewall = true;
-    enableWinbindd = false;
-    enableNmbd = false;
-    extraConfig = ''
-      server min protocol = SMB3
-      guest ok = yes
-      guest account = backup
-      map to guest = bad user
-    '';
+    winbindd.enable = false;
+    nmbd.enable = false;
   };
 }
