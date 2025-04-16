1efa7b9428711e4f144de685ead9b86a81b3b16a
davralin <gpg@davralin.work>
2025-04-16 17:10:14 +0200

    Redo impermanence so we use the module instead

--- nixos/includes/impermanence-root.nix
+++ nixos/includes/impermanence-root.nix
@@ -1,26 +1,34 @@
 { config, pkgs, ... }:
 
+let
+  impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+in
+
 {
   # Yololol
   users.users.root.initialPassword = "hunter2";
 
-  environment.etc."machine.id".source
-    = "/nix/persist/etc/machine-id";
-  environment.etc."ssh/ssh_host_rsa_key".source
-    = "/nix/persist/etc/ssh/ssh_host_rsa_key";
-  environment.etc."ssh/ssh_host_rsa_key.pub".source
-    = "/nix/persist/etc/ssh/ssh_host_rsa_key.pub";
-  environment.etc."ssh/ssh_host_ed25519_key".source
-    = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source
-    = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
-  environment.etc."NetworkManager/system-connections".source
-    = "/nix/persist/etc/NetworkManager/system-connections";
-  systemd.tmpfiles.rules = [
-    "L /var/lib/acme - - - - /nix/persist/var/lib/acme"
-    "L /var/lib/bluetooth - - - - /nix/persist/var/lib/bluetooth"
-    "L /var/lib/dnsmasq - - - - /nix/persist/var/lib/dnsmasq"
-    "L /var/lib/private/ollama - - - - /nix/persist/var/lib/private/ollama"
-    "L /var/lib/tailscale - - - - /nix/persist/var/lib/tailscale"
-  ];
+  imports = [ "${impermanence}/nixos.nix" ];
+
+  environment.persistence."/nix/persist" = {
+    hideMounts = true;
+    directories = [
+      "/etc/NetworkManager/system-connections"
+      "/etc/nixos"
+      "/opt"
+      "/var/log"
+      "/var/lib/acme"
+      "/var/lib/bluetooth"
+      "/var/lib/dnsmasq"
+      "/var/lib/nixos"
+      "/var/lib/tailscale"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+    ];
+  };
 }
