4424f5a971ccd9796067776ba4d615a86337214f
davralin <gpg@davralin.work>
2025-06-30 08:20:04 +0200

    refactor backup-k8s

--- nixos/includes/backup-k8s.nix
+++ nixos/includes/backup-k8s.nix
@@ -9,7 +9,9 @@
     createHome = true;
     group = "backup";
     home = "/opt/local";
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOiSBHUnu15cSSoC0T2QLvOFplb2WzbcxPDTBoxqpXLZ" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOiSBHUnu15cSSoC0T2QLvOFplb2WzbcxPDTBoxqpXLZ"
+      ];
   };
   users.groups.backup = {};
 }
