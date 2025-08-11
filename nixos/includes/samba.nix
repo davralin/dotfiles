8227f0e4b3f8832d3adfc1dd8e91d0a67bb8b339
davralin <gpg@davralin.work>
2025-08-11 23:24:21 +0200

    Enable autodiscovery for smb for windows

--- nixos/includes/samba.nix
+++ nixos/includes/samba.nix
@@ -3,10 +3,17 @@
 {
   # Add samba, open firewall
   # Config goes elsewhere
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-    winbindd.enable = false;
-    nmbd.enable = false;
+  services = {
+    samba = {
+      enable = true;
+      openFirewall = true;
+      winbindd.enable = false;
+      nmbd.enable = false;
+    };
+    samba-wsdd = {
+    # This enables autodiscovery on windows since SMB1 (and thus netbios) support was discontinued
+      enable = true;
+      openFirewall = true;
+    };
   };
 }
