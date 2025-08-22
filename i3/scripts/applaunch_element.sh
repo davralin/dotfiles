241a0ce5dbb1e97e382aec5ce33383b1dbb0b74a
davralin <gpg@davralin.work>
2025-08-22 14:27:56 +0200

    Don't bother with element

--- i3/scripts/applaunch_element.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -x /opt/Element/element-desktop ]; then
-  /opt/Element/element-desktop
-elif [ -x /usr/bin/flatpak ]; then
- /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/bin/element --file-forwarding im.riot.Riot @@u %U @@
-elif [ -x /usr/bin/riot-desktop ]; then
-  /usr/bin/riot-desktop
-fi
-
