c23d92eb2f919c31533c23e6ca3263b45e618d64
davralin <gpg@davralin.work>
2025-08-25 12:06:47 +0200

    Switch to always using firefox via flatpak

--- i3/scripts/applaunch_firefox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -x /opt/firefox/firefox ]; then
-  /opt/firefox/firefox
-elif [ -x /usr/bin/firefox-esr ]; then
-  /usr/bin/firefox-esr
-elif [ -x /usr/bin/flatpak ]; then
-  /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=firefox --file-forwarding org.mozilla.firefox
-fi
-
