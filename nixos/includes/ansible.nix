226602ff99d45e5fb8e7f69560e238ae95fa7520
davralin <gpg@davralin.work>
2025-10-03 23:27:06 +0200

    set homedir for ansible-user

--- nixos/includes/ansible.nix
+++ nixos/includes/ansible.nix
@@ -6,6 +6,7 @@
     isSystemUser = true;
     description = "ansible";
     group = "wheel";
+    home = "/tmp";
     shell = "${pkgs.bash}/bin/bash";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJPAMGtseoJt0rtkc2U8PM09W5AlCivFo646ZiUDnd0M" # AWX
