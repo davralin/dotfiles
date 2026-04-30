24010395e3db8140284f49de22e5e98233f4d991
davralin <gpg@davralin.work>
2026-04-30 15:41:24 +0200

    Wrong mask

--- nixos/includes/router.nix
+++ nixos/includes/router.nix
@@ -223,7 +223,7 @@
         }
         { # hiddenBox
           publicKey = "Imupq72alW84SfzxytChGYPty64loe3srAb1y+M3jjA=";
-          allowedIPs = [ "10.0.2.12/32" "10.10.0.0/24" "10.255.0.0/24" ];
+          allowedIPs = [ "10.0.2.12/32" "10.10.0.0/24" "10.255.0.0/16" ];
           persistentKeepalive = 60;
         }
         ];
