f5ea612dd1536b26747a3f11760963cb70555427
davralin <gpg@davralin.work>
2026-06-11 10:09:05 +0200

    Update docker package pre-26.05

--- nixos/includes/docker.nix
+++ nixos/includes/docker.nix
@@ -2,6 +2,7 @@
 
 {
   virtualisation.docker.enable = true;
+  virtualisation.docker.package = pkgs.docker_29;
   virtualisation.docker.autoPrune.enable = true;
   virtualisation.docker.autoPrune.dates = "monthly";
   environment.systemPackages = with pkgs; [
